d52ced840be17f4c4d13ac0728e48037889b9a1a
Carlotta Milillo <assistenza@mibtechnology.it>
2020-02-20 13:44:11 +0100

    Aggiungere i file di progetto.

---
+++ clsstudio/clsStudio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clsstudio
+{
+    public class clsStudio
+    {
+        /// <summary>
+        ///metodo che mi determina se la connessione è attiva o no 
+        /// </summary>
+        /// <returns>bool: true= conn attiva, false= conn non attiva </returns>
+        public static bool isConnected(string strconn)
+        {
+             bool bRes = false;
+            //istanzio la sql connection per aprire la connessione 
+            SqlConnection conn = new SqlConnection(strconn); //devo passargli la stringa di connessione 
+
+            //provo
+            try
+            {
+                conn.Open();
+                bRes = true;
+
+            }
+            catch (Exception ex )
+            {
+                Console.WriteLine("attention, connection failed" + ex.ToString());
+           
+            }
+            conn.Close();
+            return bRes;
+        }
+    }
+}
---
+++ studio/pgrStudio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using clsstudio;
+
+
+namespace studio
+{
+    class pgrStudio
+    {
+        public static string strConn = ConfigurationManager.AppSettings["connessione"].ToString();
+        static void Main()
+        {
+            //scrivo cosa deve succedere se la funzione mi ritorna fase o true 
+            if (!clsStudio.isConnected(strConn)) //passo la stringa di connessione al metodo
+            {
+                Console.WriteLine("connection failed");
+            }
+            else
+            {
+                Console.WriteLine("database connected succesfully");
+            }
+            Console.WriteLine("press a key to exit");
+            Console.ReadKey();
+        }
+    }
+}
